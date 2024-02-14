bbc2ce077928b8d96af51118cba2ebe1d90cb958
Akanksha Prasad <101962662+Akanksha-neural-networks@users.noreply.github.com>
2024-02-14 16:51:14 +0530

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CdkglobalModule } from './cdkglobal/cdkglobal.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [CdkglobalModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
